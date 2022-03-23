da84162ff8dcc6a0aee495c77c9e3abe4104071a
Xacor <ryabov.kostantin18@gmail.com>
2022-03-23 16:27:36 +0300

    Lesson

--- Lab1/app/app.py
+++ Lab1/app/app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 application = app
 
 @app.route("/")
 def index():
-    return "ABC"
+    msg = "Hello"
+    return render_template("index.html", message=msg)
+
+@app.route("/posts")
+def posts():
+    return render_template("posts.html", message="Посты")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", message="Об авторе")
