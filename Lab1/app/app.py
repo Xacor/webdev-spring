32262f3e45d143eaac487c759b56399adf581e86
Xacor <ryabov.kostantin18@gmail.com>
2022-03-27 21:36:24 +0300

    Lab1

--- Lab1/app/app.py
+++ Lab1/app/app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, render_template
 from faker import Faker
 
@@ -6,21 +7,36 @@ fake = Faker()
 app = Flask(__name__)
 application = app
 
-images_ids = ['2d2ab7df-cdbc-48a8-a936-35bba702def5','6e12f3de-d5fd-4ebb-855b-8cbc485278b7', '7d4e9175-95ea-4c5f-8be5-92a6b708bb3c',
-              'afc2cfe7-5cac-4b80-9b9a-d5c65ef0c728', 'cab5b7f2-774e-4884-a200-0c0180fa777f']
+images_ids = ['7d4e9175-95ea-4c5f-8be5-92a6b708bb3c',
+              '2d2ab7df-cdbc-48a8-a936-35bba702def5',
+              '6e12f3de-d5fd-4ebb-855b-8cbc485278b7',
+              'afc2cfe7-5cac-4b80-9b9a-d5c65ef0c728',
+              'cab5b7f2-774e-4884-a200-0c0180fa777f']
 
 
-def generate_post(index):
+def generate_comments(replies=True):
+    comments = []
+    for _ in range(random.randint(1, 3)):
+        comment = {'author': fake.name(), 'text': fake.text()}
+        if replies:
+            comment['replies'] = generate_comments(replies=False)
+        comments.append(comment)
+    return comments
+
+
+def generate_post(i):
     return {
         'title': 'Заголовок поста',
-        'author': fake.name(),
         'text': fake.paragraph(nb_sentences=100),
+        'author': fake.name(),
         'date': fake.date_time_between(start_date='-2y', end_date='now'),
-        'image_filename': f'{images_ids[index]}.jpg'
+        'image_id': f'{images_ids[i]}.jpg',
+        'comments': generate_comments()
     }
 
 
-posts_list = sorted([generate_post(i) for i in range(5)], key=lambda x: x['date'], reverse=True)
+posts_list = sorted([generate_post(i) for i in range(5)],
+                    key=lambda p: p['date'], reverse=True)
 
 
 @app.route('/')
@@ -30,16 +46,15 @@ def index():
 
 @app.route('/posts')
 def posts():
-    title = 'Посты'
-    return render_template('posts.html', title=title, posts=posts_list)
+    return render_template('posts.html', title='Посты', posts=posts_list)
+
 
 @app.route('/posts/<int:index>')
 def post(index):
     p = posts_list[index]
-    return render_template('post.html', post=posts_list[index], title=p['title'])
+    return render_template('post.html', title=p['title'], post=p)
 
 
 @app.route('/about')
 def about():
-    title = 'Об авторе'
-    return render_template('about.html', title=title)
+    return render_template('about.html', title='Об авторе')
