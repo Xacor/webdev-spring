99aeb22fd3d1a5ba47696ff5737dbc0cc23b8bb7
Xacor <ryabov.kostantin18@gmail.com>
2022-05-11 09:27:17 +0300

    Lab4 major fix

--- Lab4/app/app.py
+++ Lab4/app/app.py
@@ -1,35 +1,35 @@
-from urllib import request
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask import Flask, render_template, session, request, redirect, url_for, flash
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from mysqldb import MySQL
 import mysql.connector as connector
-import re 
-
-app = Flask(__name__)
-application = app
-
-mysql = MySQL(app)
+import re
 
 login_manager = LoginManager()
-login_manager.init_app(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'Для доступа к этой странице необходимо пройти процедуру аутентификации'
 login_manager.login_message_category = 'warning'
 
+app = Flask(__name__)
+application = app
+
 app.config.from_pyfile('config.py')
+
+login_manager.init_app(app)
+
+mysql = MySQL(app)
 
 CREATE_PARAMS = ['login', 'password', 'first_name',
                  'last_name', 'middle_name', 'role_id']
 
-UPDATE_PARAMS = [ 'first_name',
-                 'last_name', 'middle_name', 'role_id']
+UPDATE_PARAMS = ['first_name', 'last_name', 'middle_name', 'role_id']
 
-PASSWORD_PARAMS = ['user_id', 'password_old', 'password_new', 'password_confirm']
 
 def request_params(params_list):
     params = {}
+
     for param_name in params_list:
         params[param_name] = request.form.get(param_name) or None
+
     return params
 
 
@@ -64,21 +64,26 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
+    if request.method == 'POST':
         login_ = request.form.get('login')
-        password = request.form.get('password')
-        remember_me = request.form.get('remember_me')
+        password_ = request.form.get('password')
+        remember_me_ = request.form.get('remember_me') == 'on'
+
         with mysql.connection.cursor(named_tuple=True) as cursor:
             cursor.execute(
-                'SELECT * FROM users WHERE login=%s AND password_hash=SHA2(%s, 256);', (login_, password))
+                'SELECT * FROM users WHERE login=%s and password_hash=SHA2(%s, 256);', (login_, password_))
             db_user = cursor.fetchone()
+
         if db_user:
-            login_user(User(user_id=db_user.id, login=db_user.login),
-                       remember=remember_me)
+            login_user(
+                User(user_id=db_user.id, login=db_user.login), remember=remember_me_)
+
             flash('Вы успешно прошли процедуру аутентификации.', 'success')
             next_ = request.args.get('next')
             return redirect(next_ or url_for('index'))
+
         flash('Введены неверные логин и/или пароль.', 'danger')
+
     return render_template('login.html')
 
 
@@ -90,17 +95,21 @@ def logout():
 
 @app.route('/users')
 def users():
+
     with mysql.connection.cursor(named_tuple=True) as cursor:
         cursor.execute(
-            'SELECT users.*, roles.name AS role_name FROM users LEFT JOIN roles ON users.role_id = roles.id;')
+            'SELECT users.*, roles.name AS role_name FROM users LEFT JOIN roles ON users.role_id = roles.id;'
+        )
         users = cursor.fetchall()
+
     return render_template('users/index.html', users=users)
 
 
 @app.route('/users/new')
 @login_required
-def new():   
-    return render_template('users/new.html', user={}, roles=load_roles(), errors={})
+def new():
+    return render_template('users/new.html', errors={'login': None, 'password': None, 'last_name': None, 'first_name': None}, user={}, roles=load_roles())
+
 
 @app.route('/users/create', methods=['POST'])
 @login_required
@@ -111,7 +120,7 @@ def create():
     errors = validate_params(params)
     print(errors)
 
-    if errors['login'] != 'Ok' or errors['password'] != 'Ok' or errors['first_name'] != 'Ok' or errors['last_name'] != 'Ok':
+    if errors['login'] is not None or errors['password'] is not None or errors['first_name'] is not None or errors['last_name'] is not None:
         return render_template('users/new.html', user=params, roles=load_roles(), errors=errors)
     
     else:
@@ -144,7 +153,7 @@ def edit(user_id):
     with mysql.connection.cursor(named_tuple=True) as cursor:
         cursor.execute('SELECT * FROM users WHERE id=%s;', (user_id,))
         user = cursor.fetchone()
-    return render_template('users/edit.html', user=user, roles=load_roles(), errors={'first_name': 'Ok', 'last_name': 'Ok'})
+    return render_template('users/edit.html', user=user, errors={'login': None, 'password': None, 'last_name': None, 'first_name': None}, roles=load_roles())
 
 
 @app.route('/users/<int:user_id>/update', methods=['POST'])
@@ -157,7 +166,7 @@ def update(user_id):
     errors = {'first_name': validate_fio(params['first_name']), 'last_name': validate_fio(params['last_name'])}
     print(errors)
 
-    if errors['first_name'] != 'Ok' or errors['last_name'] != 'Ok':
+    if errors['first_name'] != 'Ok' or errors['last_name'] is not None:
         return render_template('users/edit.html', user=params, roles=load_roles(), errors=errors)
     
     else:
@@ -188,6 +197,7 @@ def delete(user_id):
     flash("Пользователь был успешно удален!", 'success')
     return redirect(url_for('users'))
 
+PASSWORD_PARAMS = ['user_id', 'password_old', 'password_new', 'password_confirm']
 
 @app.route('/users/password', methods=['GET', 'POST'])
 @login_required
@@ -208,7 +218,7 @@ def password():
         feedback['password_confirm'] = 'invalid-feedback'
 
     elif request.method == 'POST':
-        if (validate_password(params['password_new']), 'success') != 'Ok':
+        if (validate_password(params['password_new']), 'success') is not None:
             msg['password_new'] = validate_password(params['password_new'])
             validation['password_new'] = 'is-invalid'
             feedback['password_new'] = 'invalid-feedback'
@@ -235,13 +245,13 @@ def password():
         validation['password_old'] = 'is-invalid'
         feedback['password_old'] = 'invalid-feedback'
 
-    return render_template('users/password.html', msg=msg, validation=validation, feedback=feedback)
+    return render_template('password.html', msg=msg, validation=validation, feedback=feedback)
 
 def validate_login(login: str):
     #(?=.{5,25}$)[a-zA-Z0-9]+
     lenp = re.compile(r'.{5,25}')           #по хорошему надо компилировать один раз
     symbolsp = re.compile(r'[a-zA-Z0-9]+')  #по хорошему надо компилировать один раз (?=.{5,25}$)[a-zA-Z0-9]+
-    msg = 'Ok'
+    msg = None
     if not lenp.match(login):
         msg = 'Логин должен быть длиной от 5 до 25 символов!' 
     
@@ -256,7 +266,7 @@ def validate_password(password: str):
     uppercharp = re.compile(r'.*[A-ZА-Я]')
     digitp = re.compile(r'.*[0-9]')
     symbolsp = re.compile(r'[a-zA-Zа-яА-Я0-9~!?@#$%^&*\_\-+()[\]{}></\\|\"\'.,:;]+')
-    msg = 'Ok'
+    msg = None
     if not lenp.match(password):
         msg = 'Пароль должен быть длиной от 8 до 128 символов!' 
     
@@ -277,7 +287,7 @@ def validate_fio(name: str):
         return 'Поле не должно быть пусты!'
     if not p.match(name):
         return 'Допустимы только кириллические буквы!'
-    return 'Ok'
+    return None
 
 #CREATE_PARAMS = ['login', 'password', 'first_name',
 #                 'last_name', 'middle_name', 'role_id']
