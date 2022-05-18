104142d7e0351edbb775b164c410388d0390851b
Xacor <ryabov.kostantin18@gmail.com>
2022-05-18 19:02:15 +0300

    Lab6 class work

---
+++ Lab6/app/config.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'bc19682532d598bee940dfd34c91380ddde73580d16de643cce645b8cb100384'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_1611_lab6:stepbrohelpme@std-mysql.ist.mospolytech.ru/std_1611_lab6'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = False
