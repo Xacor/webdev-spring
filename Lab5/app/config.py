dfb26c9cdb6117a589190f194f14422cd47a5d8d
Xacor <ryabov.kostantin18@gmail.com>
2022-05-18 14:43:07 +0300

    Lab5

---
+++ Lab5/app/config.py
@@ -0,0 +1,5 @@
+SECRET_KEY = 'bc19682532d598bee940dfd34c91380ddde73580d16de643cce645b8cb100384'
+MYSQL_USER = 'std_1611_lab4'
+MYSQL_DATABASE = 'std_1611_lab4'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_PASSWORD = 'stepbrohelpme'
---
+++ Lab5/app/visits.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, session, request, redirect, url_for, flash, Blueprint, send_file
+from app import mysql
+import math
+import io
+import datetime
+
+bp = Blueprint('visits', __name__, url_prefix='/visits')
+
+PER_PAGE = 10
+
+def convert_to_csv(records):
+    fields = records[0]._fields
+    result = 'No,' + ','.join(fields) + '\n'
+    for i, record in enumerate(records):
+        result += f'{i+1},' + ','.join([str(getattr(record, f, '')) for f in fields]) + '\n'
+
+    return result
+
+
+def generate_report(records):
+    buffer = io.BytesIO()
+    buffer.write(convert_to_csv(records).encode(encoding='utf-8'))
+    buffer.seek(0)
+
+    return buffer
+
+
+@bp.route('/logs')
+def logs():
+    page = request.args.get('page', 1, type=int)
+
+    query = ('SELECT visit_logs.*, users.last_name, users.first_name, users.middle_name' 
+            ' FROM visit_logs LEFT JOIN users ON visit_logs.user_id = users.id' 
+            ' ORDER BY visit_logs.created_at DESC' 
+            ' LIMIT %s'
+            ' OFFSET %s;')
+
+    with mysql.connection.cursor(named_tuple=True) as cursor:
+        cursor.execute(query, (PER_PAGE, PER_PAGE*(page-1)))
+        records = cursor.fetchall()
+
+    with mysql.connection.cursor(named_tuple=True) as cursor:
+        cursor.execute('SELECT COUNT(*) AS count from visit_logs')
+        total_count = cursor.fetchone().count
+
+    total_pages = math.ceil(total_count/PER_PAGE)
+
+    return render_template('visits/logs.html', records=records, page=page, total_pages=total_pages)
+
+
+@bp.route('/stats/users')
+def users_stat():
+    query = ('SELECT users.last_name, users.first_name, users.middle_name, COUNT(*) AS count'
+            ' FROM users RIGHT JOIN visit_logs ON visit_logs.user_id = users.id' 
+            ' GROUP BY visit_logs.user_id' 
+            ' ORDER BY count DESC;')
+
+    with mysql.connection.cursor(named_tuple=True) as cursor:
+        cursor.execute(query)
+        records = cursor.fetchall()
+
+    if request.args.get('download_csv'):
+        f = generate_report(records)
+        filename = datetime.datetime.now().strftime('%d_%m_%y_%H_%M_%S') + '_users_stat.csv'
+        return send_file(f, mimetype='text/csv', as_attachment=True, attachment_filename=filename)
+
+    return render_template('visits/users_stat.html', records=records)
+
+
+@bp.route('/stats/pages')
+def pages_stat():
+    records = []
+    return render_template('visits/pages_stat.html', records=records)
