3b51de9c22f147eeb745cf1dbe3d642e20eb73b8
Xacor <ryabov.kostantin18@gmail.com>
2022-06-08 16:10:45 +0300

    Lab4 micro fix

--- Lab4/app/app.py
+++ Lab4/app/app.py
@@ -264,6 +264,7 @@ def validate_password(password: str):
     #(?=.{8,128}$)(?=.*[A-ZА-Я])(?=.*[0-9])[a-zA-Zа-яА-Я0-9~!?@#$%^&*_\-+()[\]{}></\\|\"\'.,:;]+
     lenp = re.compile(r'.{8,128}')
     uppercharp = re.compile(r'.*[A-ZА-Я]')
+    lowercahrp = re.compile(r'[a-zа-я]')
     digitp = re.compile(r'.*[0-9]')
     symbolsp = re.compile(r'[a-zA-Zа-яА-Я0-9~!?@#$%^&*\_\-+()[\]{}></\\|\"\'.,:;]+')
     msg = None
@@ -272,6 +273,9 @@ def validate_password(password: str):
     
     if not uppercharp.match(password): 
         msg = 'В пароле должна быть хотя бы одна заглавная буква!'
+
+    if not lowercahrp.match(password):
+        msg = 'В пароле должна быть хотя бы одна прописная буква!'
     
     if not digitp.match(password):
         msg = 'В пароле должна быть хотя бы одна цифра!'
