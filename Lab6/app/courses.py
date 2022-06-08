a8583769d5a38a0a304b5b039babe6ab9266f5df
Xacor <ryabov.kostantin18@gmail.com>
2022-06-08 14:50:16 +0300

    Added reviews form and reviews display

--- Lab6/app/courses.py
+++ Lab6/app/courses.py
@@ -1,16 +1,18 @@
 from flask import Blueprint, redirect, render_template, request, flash, url_for
 from app import db
-from models import Course, Category, User
+from models import Course, Category, User, Review
 from tools import CoursesFilter, ImageSaver
+from flask_login import current_user
 
 bp = Blueprint('courses', __name__, url_prefix='/courses')
 
 PER_PAGE = 3
 
 COURSE_PARAMS = ['author_id', 'name', 'category_id', 'short_desc', 'full_desc']
+REVIEW_PARAMS = ['rating', 'text']
 
-def params():
-    return {p: request.form.get(p) for p in COURSE_PARAMS }
+def params(param):
+    return {p: request.form.get(p) for p in param }
 
 def search_params():
     return {
@@ -44,21 +46,34 @@ def new():
                             users=users
                             )
 
-@bp.route('/create', methods=["POST"])
+@bp.route('/create', methods=['POST'])
 def create():
 
     f = request.files.get('background_img')
     if f and f.filename:
         img = ImageSaver(f).save()
 
-    course = Course(**params(), background_image_id = img.id)
+    course = Course(**params(COURSE_PARAMS), background_image_id = img.id)
     db.session.add(course)
     db.session.commit()
 
     flash(f'Курс {course.name} был успешно создан.')
     return redirect(url_for('courses.index'))
 
-@bp.route('/<int:course_id>')
+@bp.route('/<int:course_id>', methods=['GET', 'POST'])
 def show(course_id):
     course = Course.query.get(course_id)
-    return render_template('courses/show.html', course=course)
+    reviews = Review.query.order_by(Review.created_at).filter_by(course_id=course_id)
+    if request.method == 'POST':
+        new_review = Review(**params(REVIEW_PARAMS), course_id=course_id, user_id=current_user.id)
+        db.session.add(new_review)
+        db.session.commit()
+        flash(f'Комментарий был успешно создан.')
+        redirect(url_for('courses.show', course_id=course.id))
+
+    return render_template('courses/show.html', course=course, reviews=reviews)
+
+@bp.route('/<int:course_id>/reviews')
+def reviews(course_id):
+    reviews = Review.query.order_by(Review.created_at).filter_by(course_id=course_id)
+    return render_template('courses/reviews.html', reviews=reviews)
